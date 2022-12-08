a57f4ab321e03e1eaddc5417a6c89ed9eeaa1002
matheus_aragao <matheusaragaofs@gmail.com>
2022-12-08 20:56:00 -0300

    adding tailwind config and react native navigation

---
+++ screens/Test.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Test = () => {
+    return (
+    <View className='bg-red-200'>
+      <Text className='text-blue-500'>Test Screen with tailwind</Text>
+    </View>
+  )
+}
+
+export default Test
