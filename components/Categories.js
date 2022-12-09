d10d357ee0407644289663300a77de32bcedd123
matheus_aragao <matheusaragaofs@gmail.com>
2022-12-09 11:49:51 -0300

    adding  categoryCard

--- components/Categories.js
+++ components/Categories.js
@@ -4,8 +4,42 @@ import CategoryCard from "./CategoryCard";
 
 const Categories = () => {
   return (
-    <ScrollView>
-      <CategoryCard />
+    <ScrollView
+      contentContainerStyle={{
+        paddingHorizontal: 15,
+        paddingTop: 10,
+      }}
+      horizontal
+      showsHorizontalScrollIndicator={false}
+    >
+      <CategoryCard
+        title="Testing 1"
+        imgUrl="https://links.papareact.com/gn7"
+      />
+      <CategoryCard
+        title="Testing 2 "
+        imgUrl="https://links.papareact.com/gn7"
+      />
+      <CategoryCard
+        title="Testing 3"
+        imgUrl="https://links.papareact.com/gn7"
+      />
+      <CategoryCard
+        title="Testing 3"
+        imgUrl="https://links.papareact.com/gn7"
+      />
+      <CategoryCard
+        title="Testing 3"
+        imgUrl="https://links.papareact.com/gn7"
+      />
+      <CategoryCard
+        title="Testing 3"
+        imgUrl="https://links.papareact.com/gn7"
+      />
+      <CategoryCard
+        title="Testing 3"
+        imgUrl="https://links.papareact.com/gn7"
+      />
     </ScrollView>
   );
 };
