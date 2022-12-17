16da6dc44dec4afdc0b28c9fbe74fcf0d5529003
Matheus Aragao <matheusaragaofs@gmail.com>
2022-12-17 18:03:48 -0300

    implementing redux

--- App.js
+++ App.js
@@ -1,18 +1,23 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Provider } from "react-redux";
 const Stack = createNativeStackNavigator();
 
 //test
 
 import { Home, Restaurant } from "./screens";
+import { store } from "./store";
 
 export default function App() {
   return (
     <NavigationContainer>
+      <Provider store={store}>
+
       <Stack.Navigator>
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Restaurant" component={Restaurant} />
       </Stack.Navigator>
+      </Provider>
     </NavigationContainer>
   );
 }
